7be92a026031f5cc18628bf7318505b9935d5b4c
Agnieszka Kozubek-Krycuń <a.kozubek@wp.pl>
2018-02-09 09:30:36 +0100

    Advent of Code 2017 - Day 4

---
+++ advent-of-code/2017/4-passphrase.py
@@ -0,0 +1,44 @@
+import unittest
+
+def isvalid(passphrase):
+    words = passphrase.split()
+    return len(set(words)) == len(words)
+
+def isvalid2(passphrase):
+    words = passphrase.split()
+    canonic_words = [''.join(sorted(w)) for w in words]
+    return len(set(canonic_words)) == len(canonic_words)
+
+def countvalid(filename, fun):
+    count = 0
+    with open(filename) as file:
+        for line in file:
+            if fun(line):
+                 count += 1
+    return count
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(isvalid('aa bb cc dd ee'), True)
+    def test2(self):
+        self.assertEqual(isvalid('aa bb cc dd aa'), False)
+    def test3(self):
+        self.assertEqual(isvalid('aa bb cc dd aaa'), True)
+
+
+    def test21(self):
+        self.assertEqual(isvalid2('abcde fghij'), True)
+    def test22(self):
+        self.assertEqual(isvalid2('abcde xyz ecdab'), False)
+    def test23(self):
+        self.assertEqual(isvalid2('a ab abc abd abf abj'), True)
+    def test24(self):
+        self.assertEqual(isvalid2('iiii oiii ooii oooi'), True)
+    def test25(self):
+        self.assertEqual(isvalid2('oiii ioii iioi iiio'), False)
+
+
+print('Result:', countvalid('day4.txt', isvalid))
+print('Result:', countvalid('day4.txt', isvalid2))
+unittest.main()
