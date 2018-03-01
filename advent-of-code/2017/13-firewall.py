711cc38f39d8d5827fcbf450ab2679495f7342d1
Agnieszka Kozubek-Krycuń <a.kozubek@wp.pl>
2018-03-01 20:07:25 +0100

    Advent of Code, Part 2

--- advent-of-code/2017/13-firewall.py
+++ advent-of-code/2017/13-firewall.py
@@ -41,16 +41,68 @@ def severity(spec):
        # move scanners
        for l in layers:
            move(l)
-
-       # move packet
    return sev
+
+
+def create_layers(spec):
+    layers = list()
+    index = 0
+    for line in spec:
+        [layer, depth] = [ int(l) for l in (line.split(": ")) ]
+#        print(layer, depth)
+#        print(index)
+        for i in range(index, layer): 
+            layers.append([])
+            index += 1
+        layers.append([0] * depth)
+#        print(layers)
+        layers[layer][0] = 1
+        index += 1
+    return layers
+
+def severity2(spec):
+    # create layers
+    layers = create_layers(spec)
+
+    delays = []
+    picosecond = 0
+    while True:
+        print("Picosecond:", picosecond)
+        delays.append(picosecond)
+        # print("Delays:", delays)
+        # check if any delays are caught
+        for d in delays[:]:
+            # gdzie w tej picosekundzie jest pakiet wyruszajacy z opoznieniem
+            # d
+            position = picosecond - d
+            if position == len(layers):
+                 # znalezlismy?
+                 return d
+           
+            if len(layers[position]) > 0 and layers[position][0] != 0:
+                # caught!
+                delays.remove(d)
+
+        # move scanners
+        for l in layers:
+            move(l)
+        picosecond += 1
+
 
 class Test(unittest.TestCase):
     def test1(self):
         input = ['0: 3','1: 2', '4: 4', '6: 4']
         self.assertEqual(severity(input), 24)
 
+    def test2(self):
+        input = ['0: 3','1: 2', '4: 4', '6: 4']
+        self.assertEqual(severity2(input), 10)
+
 with open("day13.txt") as file:
     print("Result:", severity(file.readlines()))
+
+with open("day13.txt") as file:
+     print("Result:", severity2(file.readlines()))
+     pass
 
 unittest.main()
