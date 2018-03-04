f072f9a4027a4b5b6378a1b1ee51b3564fb48f0e
Agnieszka Kozubek-Krycuń <a.kozubek@wp.pl>
2018-03-04 19:28:20 +0100

    Advent of Code, Day 14, Part 2

--- advent-of-code/2017/14-disk-defrag.py
+++ advent-of-code/2017/14-disk-defrag.py
@@ -85,12 +85,48 @@ def disk_count(input):
             sum += int(s)
     return sum
 
+def neighbors(square, squares):
+    (x,y) = square
+    neighbors = set()
+    for i in range(-1,1):
+        for j in range(-1,1):
+            nx = x + i
+            ny = y + j
+            if i * j == 0 and 0 <= nx < 128 and 0 <= ny < 128:
+                 if (nx, ny) in squares:
+                     neighbors.add((nx, ny))
+    return neighbors 
+
+def disk_groups(input):
+    binary = disk(input)
+    squares = dict()
+    for i in range(0, 128):
+        for j in range(0, 128):
+            if binary[i][j] == '1':
+                 squares[(i,j)] = { (i,j) }
+    for square in squares:
+        ns = neighbors(square, squares)
+        squares[square].update(ns)
+        for n in squares[square].copy():
+            squares[square].update(squares[n])
+        for n in squares[square]:
+            squares[n].update(squares[square])
+
+    sorted_groups = [ sorted(group) for group in squares.values() ]
+    labels = []
+    for group in sorted_groups:
+        labels.append(",".join([str(t) for t in group ]))
+    return len(set(labels))
 
 class Test(unittest.TestCase):
     def test1(self):
         self.assertEqual(disk_count('flqrgnkx'), 8108)
 
+    def test2(self):
+        self.assertEqual(disk_groups('flqrgnkx'), 1242)
+
 input = 'nbysizxe'
 print("Result:", disk_count(input))
+print("Result 2:", disk_groups(input))
 
 unittest.main()
